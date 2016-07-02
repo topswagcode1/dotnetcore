806b5b1aa145d7c8d6ce12ea8b00010c0981022c
kiksen1987 <joshua.ryder@outlook.com>
2016-07-02 16:33:55 +0200

    Small DotNet Core Web App

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace topswagcodeapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:8080")
+                .Build();
+
+            host.Run();
+            Console.WriteLine("Hello Web World!");
+        }
+    }
+}
