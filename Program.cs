0d3233507c789b115d8072b6c96025c28b236ff0
topswagcode1 <51211671+topswagcode1@users.noreply.github.com>
2019-09-09 12:21:09 +0200

    yery

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Linq;  
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace dotnetcore
 {
-    public class Program
+    public class Program  
     {
         public static void Main(string[] args)
         {
